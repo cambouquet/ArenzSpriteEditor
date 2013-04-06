d4a5b0a1de9815877c153d716e0f33d4041482bd
Camille Bouquet <bouquetcamille@hotmail.fr>
2013-04-06 16:51:33 +0200

    renames PTextField into LabelTextField and removes extention

---
+++ com/arenz/spriteeditor/ui/dialogs/elements/LabelTextField.java
@@ -0,0 +1,17 @@
+package com.arenz.spriteeditor.ui.dialogs.elements;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ * @author Camille
+ *
+ */
+public class LabelTextField {
+	private JLabel label;
+	private JTextField textField;
+	
+	public LabelTextField(String label) {
+		
+	}
+}
